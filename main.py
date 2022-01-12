9649dd824092b1a7c2a8c75eea6489b18b95f2ad
Indrashis Mitra <indrashismitra2000@gmail.com>
2022-01-12 12:10:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+     print ("Bill Splitter") 
+  
+billAmount = int (input ("Enter the total bill amount : ")) 
+ 
+     no =
+       int (input
+	    ("How many people are at the table? Enter response as a whole number: "))
+  
+ 
+splitPerPerson =
+  billAmount / no 
+ 
+print ("Each person must pay:", splitPerPerson)
